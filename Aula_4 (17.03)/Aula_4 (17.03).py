d4ae9fa7eaea4bcef803c601a0534872912ef0a7
ThiagoNienov <thiago.nienov@apollusehs.com.br>
2023-03-17 19:36:05 -0300

    aula4

---
+++ Aula_4 (17.03)/Aula_4 (17.03).py
@@ -0,0 +1,13 @@
+
+
+def multiplica(v1, v2):
+    produto = v1 * v2
+    return produto
+
+
+v1 = int(input("Valor 1: "))
+v2 = int(input("Valor 2: "))
+res = multiplica(v1, v2)
+print(res)
+#print(multiplica(v1, v2))
+
